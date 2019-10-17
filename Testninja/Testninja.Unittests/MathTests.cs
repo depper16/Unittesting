4da9d7e80d3a0f3b8362f4f6e249cbe7d479744a
depper16 <56670876+depper16@users.noreply.github.com>
2019-10-17 15:09:17 +0700

    2_6WritingaSimpleUnitest

---
+++ Testninja/Testninja.Unittests/MathTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace Testninja.Unittests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
