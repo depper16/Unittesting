cbeafdd2fb87b9c29e66710040f49e92b7d45200
depper16 <56670876+depper16@users.noreply.github.com>
2019-10-17 14:31:13 +0700

    1_7WritingYourFirstUnitTest

---
+++ Testninja/Testninja.Unittests/ReservationTests.cs
@@ -0,0 +1,23 @@
+﻿using TestNinja.Fundamentals;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
