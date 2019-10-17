1b9efac1a94792d2f91ba274e6d3c7684147f272
depper16 <56670876+depper16@users.noreply.github.com>
2019-10-17 15:56:33 +0700

    3_3TestingArraysandCollections

--- Testninja/Testninja.Unittests/MathTests.cs
+++ Testninja/Testninja.Unittests/MathTests.cs
@@ -21,7 +21,6 @@ namespace Testninja.Unittests
         {
             var result = _math.Add(1, 2);
 
-            //            Assert.That(result, Is.EqualTo(3));
             Assert.That(_math, Is.Not.Null);
         }
         [Test]
@@ -33,6 +32,26 @@ namespace Testninja.Unittests
             var result = _math.Max(a, b);
 
             Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            //            Assert.That(result, Is.Not.Empty);
+
+            //            Assert.That(result.Count(), Is.EqualTo(3));
+
+            //            Assert.That(result, Does.Contain(1));
+            //            Assert.That(result, Does.Contain(3));
+            //            Assert.That(result, Does.Contain(5));
+
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5 }));
+
+            //            Assert.That(result, Is.Ordered);
+            //            Assert.That(result, Is.Unique);
+
         }
     }
 }
