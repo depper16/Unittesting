53094c3d27d0dd65a874c2661566a49ea6b24ba7
depper16 <56670876+depper16@users.noreply.github.com>
2019-10-19 03:41:03 +0700

    7_6WritingtheSecondTest

--- Testninja/Testninja.Unittests/mocking/BookingHelperTests.cs
+++ Testninja/Testninja.Unittests/mocking/BookingHelperTests.cs
@@ -36,10 +36,22 @@ namespace TestNinja.UnitTests.Mocking
             {
                 Id = 1,
                 ArrivalDate = Before(_existingBooking.ArrivalDate, days: 2),
-                DepartureDate = After(_existingBooking.ArrivalDate),
+                DepartureDate = Before(_existingBooking.ArrivalDate),
             }, _repository.Object);
 
             Assert.That(result, Is.Empty);
+        }
+        [Test]
+        public void BookingStartsBeforeAndFinishesInTheMiddleOfAnExistingBooking_ReturnExistingBookingsReference()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = Before(_existingBooking.ArrivalDate),
+                DepartureDate = After(_existingBooking.ArrivalDate),
+            }, _repository.Object);
+
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
         }
         private DateTime Before(DateTime dateTime, int days = 1)
         {
